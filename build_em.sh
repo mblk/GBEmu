83901c2f3692f8a32d78e7fe4bf95de9a21fd819
Marcel Block <marcelblock@gmail.com>
2018-05-09 01:22:10 +0200

    Minor changes to build_em.sh for easier testing.

--- build_em.sh
+++ build_em.sh
@@ -3,7 +3,7 @@
 mkdir -p build_em
 
 time emcc src/*.cc src/emulator/*.cc \
-    -o build_em/gbemu.html \
+    -o build_em/gbemu_asmjs.html \
     -std=c++1z \
     -s USE_SDL=2 \
     --embed-file tetris.gb \
@@ -12,15 +12,18 @@ time emcc src/*.cc src/emulator/*.cc \
     -s USE_PTHREADS=0 \
     -s DISABLE_EXCEPTION_CATCHING=0
 
-# time emcc src/*.cc src/emulator/*.cc \
-#     -o build_em/gbemu2.html \
-#     -std=c++1z \
-#     -s USE_SDL=2 \
-#     --embed-file tetris.gb \
-#     -O2 \
-#     -s WASM=1 \
-#     -s USE_PTHREADS=0 \
-#     -s DISABLE_EXCEPTION_CATCHING=0
+#time emcc src/*.cc src/emulator/*.cc \
+#    -o build_em/gbemu_wasm.html \
+#    -std=c++1z \
+#    -s USE_SDL=2 \
+#    --embed-file tetris.gb \
+#    -O2 \
+#    -s WASM=1 \
+#    -s USE_PTHREADS=0 \
+#    -s DISABLE_EXCEPTION_CATCHING=0
 
 
 #--embed-file mario.gb \
+
+rm /var/www/html/gbemu/* -v
+cp build_em/* /var/www/html/gbemu -v
